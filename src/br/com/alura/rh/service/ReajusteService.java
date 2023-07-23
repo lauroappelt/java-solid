5d72455b0e0bcc6444171c1efaaece6b550d7315
lauro <lauro@solis.com.br>
2023-07-23 14:55:11 -0300

    Aplicando o principio OCP, classe ReajusteService se torna aberta para expansão e fechad para modificação, novas validações serão criadas sem modificar a classe de reajuste.

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,20 +1,20 @@
 package br.com.alura.rh.service;
 
-import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.interfaces.ValidacaoReajuste;
 import br.com.alura.rh.model.Funcionario;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
+import java.util.List;
 
 public class ReajusteService {
-    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+    private List<ValidacaoReajuste> validacoes;
 
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
